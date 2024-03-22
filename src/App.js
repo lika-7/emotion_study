cb8adb33528ed73c967c218565381401eff2efef
lika-7 <dhwhdtn93@gmail.com>
2024-03-22 23:54:05 +0900

    make book detail page

--- src/App.js
+++ src/App.js
@@ -1,62 +1,24 @@
-/** @jsxImportSource @emotion/react */
-
-import { css, Global, ThemeProvider,useTheme } from '@emotion/react';
-import { BrowserRouter,Routes,Route, Outlet } from 'react-router-dom';
-import BookDetailPage from './pages/BookDetailPage';
-import SearchPage from './pages/SearchPage';
-import Footer from './components/Footer';
-import { useState } from 'react';
-import { themeDark, themeLight } from './components/Theme';
-
-
-const Layout = ({isDark, setIsDark}) => {
-  const theme = useTheme();
-  return (
-    <div>
-      <Global
-        styles={css`
-        body {
-          background-color: ${theme.background};
-          color: ${theme.text};
-          transition-duration: 0.2s;
-          transition-property: background-color, color;
-        }
-        a {
-          color: ${theme.text};
-          text-decoration: none;
-        }
-        ul {
-          list-style: none;
-          padding: 0;
-        }
-      `}
-      />
-      <div
-        css={css`
-          min-height: 90vh;
-        `}
-      >
-        <Outlet />
-      </div>
-      <Footer isDark={isDark} setIsDark={setIsDark}/>
-    </div>
-  );
-};
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [isDark, setIsDark] = useState(false);
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={isDark ? themeDark : themeLight}>
-        <Routes>
-          <Route path="/" element={<Layout isDark={isDark} setIsDark={setIsDark}/>}>
-            <Route index element={<SearchPage />} />
-            <Route path="/book/:bookId" element={<BookDetailPage />} />
-          </Route>
-        </Routes>
-      </ThemeProvider>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
